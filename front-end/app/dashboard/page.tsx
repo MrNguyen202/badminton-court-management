aca106464abc7a07df2c40b3dfbe3ce8b42bb4e1
nguyenthuan202 <nguyenthuan020010@gmail.com>
2025-02-26 03:34:46 +0700

    fix: change UI list court

--- front-end/app/dashboard/page.tsx
+++ front-end/app/dashboard/page.tsx
@@ -2,21 +2,11 @@
 
 import React, { useEffect, useState } from "react";
 import noImage from "../../public/no-product-image.jpg";
+import location from "../../public/location.png";
+import courtImage from "../../public/football-field.gif";
+import calenderImage from "../../public/calendar.gif";
+import utilityImage from "../../public/utility.gif";
 import { getAllCourt } from "../api/court-services/courtAPI";
-
-// const courts = [
-//   { id: 1, name: "Sân Cầu Lông A", address: "123 Đường A, Quận 1", price: "100.000 VND/h", image: null, rating: 4.5, district: 1 },
-//   { id: 2, name: "Sân Cầu Lông B", address: "456 Đường B, Quận 3", price: "120.000 VND/h", image: "https://picsum.photos/200/321", rating: 4.0, district: 3 },
-//   { id: 3, name: "Sân Cầu Lông C", address: "789 Đường C, Quận 5", price: "90.000 VND/h", image: "https://picsum.photos/200/322", rating: 4.2, district: 5 },
-//   { id: 4, name: "Sân Cầu Lông D", address: "101 Đường D, Quận 7", price: "110.000 VND/h", image: "https://picsum.photos/200/323", rating: 3.8, district: 7 },
-//   { id: 5, name: "Sân Cầu Lông E", address: "202 Đường E, Quận 10", price: "130.000 VND/h", image: "https://picsum.photos/200/324", rating: 4.7, district: 10 },
-//   { id: 6, name: "Sân Cầu Lông F", address: "303 Đường F, Quận 2", price: "95.000 VND/h", image: "https://picsum.photos/200/325", rating: 4.1, district: 2 },
-//   { id: 7, name: "Sân Cầu Lông G", address: "404 Đường G, Quận 4", price: "105.000 VND/h", image: "https://picsum.photos/200/326", rating: 4.3, district: 4 },
-//   { id: 8, name: "Sân Cầu Lông H", address: "505 Đường H, Quận 6", price: "115.000 VND/h", image: "https://picsum.photos/200/327", rating: 4.6, district: 6 },
-//   { id: 9, name: "Sân Cầu Lông I", address: "606 Đường I, Quận 8", price: "125.000 VND/h", image: "https://picsum.photos/200/328", rating: 4.0, district: 8 },
-//   { id: 10, name: "Sân Cầu Lông J", address: "707 Đường J, Quận 9", price: "135.000 VND/h", image: "https://picsum.photos/200/329", rating: 4.8, district: 9 },
-//   { id: 11, name: "Sân Cầu Lông K", address: "808 Đường K, Quận 11", price: "85.000 VND/h", image: "https://picsum.photos/200/330", rating: 3.9, district: 11 },
-// ];
 
 type Court = {
   id: number;
@@ -109,30 +99,43 @@ function BadmintonCourtList() {
           </select>
         </div>
         <div className="w-4/5 p-4">
+          <h1 className="text-3xl font-bold mb-4">Danh sách sân cầu lông</h1>
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
             {currentCourts.map((court) => (
               <div key={court.id} className="border p-4 rounded-lg shadow-md bg-white">
                 <img src={court.images && court.images[0] ? court.images[0].url : noImage.src} alt={"No image"} className="w-full h-52 object-cover rounded-md mb-2" />
-                <div className="flex items-center justify-between">
-                  <h2 className="text-lg font-semibold">{court.name}</h2>
+                <h2 className="text-lg font-semibold mb-4">{court.name}</h2>
+                <div className="flex items-center">
+                  <img src={location.src} alt="location" className="w-5 h-5 mr-1" />
+                  <p className="text-gray-600">Khu vực: {court.address}</p>
+                </div>
+                <div className="flex justify-between">
+                  <div className="flex items-center">
+                    <img src={courtImage.src} alt="location" className="w-5 h-5 mr-1" />
+                    <p className="text-gray-600">Số sân: {court.numberOfCourts}</p>
+                  </div>
                   <p className="text-yellow-500 font-semibold">
                     {Array.from({ length: Math.floor(court.numberOfCourts) }, (_, i) => (
                       <span key={i}>⭐</span>
                     ))}
                     {court.numberOfCourts % 1 !== 0 && <span>⭐</span>}
+                    ({court.numberOfCourts})
                   </p>
                 </div>
-                <p className="text-gray-600">{court.address}</p>
-                <p className="font-bold text-red-500">Giá: {court.price}</p>
-                <div className="flex flex-col-2 justify-center items-center">
-                  <button className="mt-2 mr-2 bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600 w-full" onClick={() => handleViewDetails(court)}>Xem chi tiết</button>
-                  <button
-                    className="mt-2 ml-2 bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 w-full"
-                    onClick={() => handleBooking(court)}
-                  >
-                    Đặt sân
-                  </button>
+                <div className="flex">
+                  <img src={calenderImage.src} alt="location" className="w-5 h-5 mr-1" />
+                  <p className="text-gray-600">Sân trống: </p>
                 </div>
+                <div className="flex">
+                  <img src={utilityImage.src} alt="location" className="w-5 h-5 mr-1" />
+                  <p className="text-gray-600">Tiện ích: {court.price}</p>
+                </div>
+                <button
+                  className="mt-3 bg-slate-900 border-slate-900 border-1 text-white px-4 py-2 rounded-md hover:bg-white hover:text-black transition duration-500 w-full"
+                  onClick={() => handleBooking(court)}
+                >
+                  Đặt sân ngay
+                </button>
               </div>
             ))}
           </div>
