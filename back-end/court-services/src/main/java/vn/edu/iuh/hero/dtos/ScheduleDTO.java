62a59d7457767f9dadfbe2ff159b62e12bdb7244
NguyenThanhThuan <nguyenthuan020010@gmail.com>
2025-03-03 22:39:07 +0700

    fix: fix feat insert schedule

---
+++ back-end/court-services/src/main/java/vn/edu/iuh/hero/dtos/ScheduleDTO.java
@@ -0,0 +1,17 @@
+/*
+ * @ (#) ScheduleDTO.java    1.0    3/3/2025
+ *
+ *
+ */
+ 
+package vn.edu.iuh.hero.dtos;
+/*
+ * @Description: 
+ * @Author: Nguyen Thanh Thuan
+ * @Date: 3/3/2025
+ * @Version: 1.0
+ *
+ */
+ 
+public class ScheduleDTO {
+}
