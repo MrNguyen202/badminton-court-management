73d661029f7b6de4285a4d0addf41f7652becf81
Bao44 <tqbao44@gmail.com>
2025-04-04 14:31:54 +0700

    create booking-service

---
+++ back-end/booking-services/src/test/java/vn/edu/iuh/fit/bookingservices/BookingServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.edu.iuh.fit.bookingservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
